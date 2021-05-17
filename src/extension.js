0c845336e688b8c3a27beb2ccfe7ebc06c333f9e
Julian Schacher <julian@jsts.xyz>
2021-05-17 14:37:57 +0200

    Other: License this project
    License this project under the GNU General Public License version 3.

--- src/extension.js
+++ src/extension.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Top-Bar-Organizer (a Gnome Shell Extension for
+ * organizing your Gnome Shell top bar).
+ * Copyright (C) 2021 Julian Schacher
+ *
+ * Top-Bar-Organizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 "use strict";
 
 const ExtensionUtils = imports.misc.extensionUtils;
